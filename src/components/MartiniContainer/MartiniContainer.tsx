cb97d73df71f90ee50fc907aa8ec95519b83649c
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-07-27 14:16:05 -0600

    Change Specialty component to FC and MartiniContainer to Class Component

--- src/components/MartiniContainer/MartiniContainer.tsx
+++ src/components/MartiniContainer/MartiniContainer.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react"
+import React, { Component } from "react"
 import "./MartiniContainer.css"
 import MartiniImage from "../../../src/Martini.jpeg"
 import MartiniDeck from "../MartiniDeck/MartiniDeck"
@@ -14,51 +14,75 @@ type SpecialtyDrink = {
     idDrink: string;
     strDrink: string;
     strDrinkThumb: string;
-    strInstructions?: string | null;
-    strIngredient1?: string | null;
-    strIngredient2?: string | null;
-    strIngredient3?: string | null;
-    strIngredient4?: string | null;
-    strIngredient5?: string | null;
-    strIngredient6?: string | null;
-    strIngredient7?: string | null;
-    strIngredient8?: string | null;
-    strIngredient9?: string | null;
-    strIngredient10?: string | null;
-    strIngredient11?: string | null;
+    // strInstructions?: string | null;
+    // strIngredient1?: string | null;
+    // strIngredient2?: string | null;
+    // strIngredient3?: string | null;
+    // strIngredient4?: string | null;
+    // strIngredient5?: string | null;
+    // strIngredient6?: string | null;
+    // strIngredient7?: string | null;
+    // strIngredient8?: string | null;
+    // strIngredient9?: string | null;
+    // strIngredient10?: string | null;
+    // strIngredient11?: string | null;
 };
 
-const MartiniContainer: FC<SpecialtyState> = ({ martiniDrinks }) => {
+class MartiniContainer extends Component {
+    constructor(props: any) {
+         super(props)
+         this.state = {
+             martiniDrinks:[],
+            // amarettoDrinks:[],
+            // bourbonDrinks:[]
+         }
+     }
+    //  getMartinis = () => {
+    //      let allMartinis = this.state.martiniDrinks.map((martini: SpecialtyDrink) => {
+    //          return (
+    //              <MartiniDeck
+    //                  idDrink={martini.idDrink}
+    //                  strDrink={martini.strDrink}
+    //                  strDrinkThumb={martini.strDrinkThumb}
+                 // strInstructions={martini.strInstructions}
+                 // strIngredient1={martini.strIngredient1}
+                 // strIngredient2={martini.strIngredient2}
+                 // strIngredient3={martini.strIngredient3}
+                 // strIngredient4={martini.strIngredient4}
+                 // strIngredient5={martini.strIngredient5}
+                 // strIngredient6={martini.strIngredient6}
+                 // strIngredient7={martini.strIngredient7}
+                 // strIngredient8={martini.strIngredient8}
+                 // strIngredient9={martini.strIngredient9}
+                 // strIngredient10={martini.strIngredient10}
+                 // strIngredient11={martini.strIngredient11}
+    //              />);
+    //      })
+    //  }
 
-   let allMartinis =  martiniDrinks.map((martini: SpecialtyDrink) => {
-            return (
-            <MartiniDeck
-                idDrink={martini.idDrink}   
-                strDrink={martini.strDrink}
-                strDrinkThumb={martini.strDrinkThumb}
-                // strInstructions={martini.strInstructions}
-                // strIngredient1={martini.strIngredient1}
-                // strIngredient2={martini.strIngredient2}
-                // strIngredient3={martini.strIngredient3}
-                // strIngredient4={martini.strIngredient4}
-                // strIngredient5={martini.strIngredient5}
-                // strIngredient6={martini.strIngredient6}
-                // strIngredient7={martini.strIngredient7}
-                // strIngredient8={martini.strIngredient8}
-                // strIngredient9={martini.strIngredient9}
-                // strIngredient10={martini.strIngredient10}
-                // strIngredient11={martini.strIngredient11}
-            />);
-            })
-        
-            return(
-                <div>
-                   {/* {allMartinis} */}
-                    <div className="martiniButton">
-                        <img className="martiniImage" src={MartiniImage} />
-                        <h3>10 Martinis that will impress your guests</h3>
-                    </div>
+     render( ) {
+        return(
+            <div>
+                {/* {allMartinis} */}
+                <div className="martiniButton">
+                    <img className="martiniImage" src={MartiniImage} />
+                    <h3>10 Martinis that will impress your guests</h3>
                 </div>
-            )
+            </div>
+        )
+     }
+
+
+ 
+        
+            //return(
+                // <div>
+                //     {/* {allMartinis} */}
+                //     <div className="martiniButton">
+                //         <img className="martiniImage" src={MartiniImage} />
+                //         <h3>10 Martinis that will impress your guests</h3>
+                //     </div>
+                // </div>
+            //)
 }
 export default MartiniContainer
--- src/components/SpecialtyContainer/SpecialtyContainer.tsx
+++ src/components/SpecialtyContainer/SpecialtyContainer.tsx
@@ -1,56 +1,72 @@
-import React, { Component } from "react"
+import { FC } from "react"
 import"./SpecialtyContainer.css"
 import MartiniContainer from "../MartiniContainer/MartiniContainer"
 import { fetchSpecialtyCocktails } from "../../apiCalls"
 import MartiniImage from "../../../src/Martini.jpeg"
 
-type SpecialtyState = {
-    martiniDrinks: Drink[]
-    // amarettoDrinks: Drink[]
-    // bourbonDrinks: Drink[]
-}
-type Drink = {
-    idDrink: string;
-    strDrink: string;
-    strDrinkThumb: string;
-    strInstructions?: string | null;
-    strIngredient1?: string | null;
-    strIngredient2?: string | null;
-    strIngredient3?: string | null;
-    strIngredient4?: string | null;
-    strIngredient5?: string | null;
-    strIngredient6?: string | null;
-    strIngredient7?: string | null;
-    strIngredient8?: string | null;
-    strIngredient9?: string | null;
-    strIngredient10?: string | null;
-    strIngredient11?: string | null;
-};
+// type SpecialtyState = {
+//     martiniDrinks: Drink[]
+//     // amarettoDrinks: Drink[]
+//     // bourbonDrinks: Drink[]
+// }
+// type Drink = {
+//     idDrink: string;
+//     strDrink: string;
+//     strDrinkThumb: string;
+    // strInstructions?: string | null;
+    // strIngredient1?: string | null;
+    // strIngredient2?: string | null;
+    // strIngredient3?: string | null;
+    // strIngredient4?: string | null;
+    // strIngredient5?: string | null;
+    // strIngredient6?: string | null;
+    // strIngredient7?: string | null;
+    // strIngredient8?: string | null;
+    // strIngredient9?: string | null;
+    // strIngredient10?: string | null;
+    // strIngredient11?: string | null;
+//};
 
-class SpecialtyContainer extends Component<{}, SpecialtyState> {
-    constructor(props:any) {
-        super(props)
-        this.state = {
-            martiniDrinks:[],
+const SpecialtyContainer: FC = () => {
+
+    return(
+        <div className="specialtyContainer">
+            <MartiniContainer />
+        </div>
+    )
+}
+
+export default SpecialtyContainer
+
+
+
+
+
+
+
+
+// class SpecialtyContainer extends Component<{}, SpecialtyState> {
+//     constructor(props:any) {
+//         super(props)
+//         this.state = {
+//             martiniDrinks:[],
             // amarettoDrinks:[],
             // bourbonDrinks:[]
-        }
-    }
+    //     }
+    // }
 
-    componentDidMount = () => {
-        fetchSpecialtyCocktails()
-        .then(data => {
-            this.setState({ martiniDrinks: data.drinks})
-            console.log("martini data array", data.drinks);
-        })
-    }
+    // componentDidMount = () => {
+    //     fetchSpecialtyCocktails()
+    //     .then(data => {
+    //         this.setState({ martiniDrinks: data.drinks})
+    //         console.log("martini data array", data.drinks);
+    //     })
+    // }
 
-    render() {
-        return (
-            <div className="specialtyContainer">
-                <MartiniContainer martiniDrinks={this.state.martiniDrinks}/>
-            </div>
-        )
-    }
-}
-export default SpecialtyContainer
+    // render() {
+    //     return (
+            // <div className="specialtyContainer">
+            //     <MartiniContainer martiniDrinks={this.state.martiniDrinks}/>
+            // </div>
+    //     )
+    // }
